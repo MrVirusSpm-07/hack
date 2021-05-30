f68344d38b48e029d0d9f1e9b2b9bbb6868558b2
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-05-30 21:09:44 +0700

    Add files via upload

---
+++ cans.sh
@@ -0,0 +1,41 @@
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+echo $green
+    figlet MR VIRUS
+echo
+sleep 1
+  echo $blue"=========================================="
+  echo $purple"(+) AUTHOR : MR VIRUS SPM              (+)"
+  echo "(+) TEAM   : MAVIA TEKNOLOGI INDONESIA (+)"
+  echo "(+) KONTAK : +62 882-1235-2864         (+)"
+  echo $blue"=========================================="
+sleep 2
+       echo "# CIE JOMBLO YAH NIH ADA KONTAK CEWE CANTIK"
+       echo
+sleep 1
+     echo $purple"1.) kontak cowok"
+     echo $white"2.) kontak cewek"
+read -p "======> " yit
+if [ $yit = 2 ]
+then
+clear
+     echo
+      echo "loading.............."
+      sleep 3
+git clone https://github.com/mrvirusrecord/mrvirus
+fi
+if [ $yit = 1 ]
+then
+clear
+     sleep 2
+     figlet GAK ADA
+     figlet ELU MAU
+     figlet HOMO:V
+fi
